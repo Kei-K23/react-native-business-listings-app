34af05c5f35dbdf2f4fde278ac824f9b13817456
Kei-K23 <keiksl2301@gmail.com>
2024-06-09 18:26:42 +0630

    Add loading indicator when data fetching

--- app/businessLists/[category].tsx
+++ app/businessLists/[category].tsx
@@ -1,19 +1,22 @@
 import BusinessListItem from "@/components/BusinessLists/BusinessListItem";
 import { db } from "@/config/firebase";
 import { AppStyle } from "@/constants/AppStyle";
+import { Colors } from "@/constants/Colors";
 
 import { Business } from "@/types";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 
 export default function BusinessListsByCategory() {
   const navigation = useNavigation();
   const { category } = useLocalSearchParams<{ category: string }>();
   const [businesses, seBusinesses] = useState<Business[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>();
 
   const getBusinesses = async () => {
+    setIsLoading(true);
     seBusinesses([]);
 
     const q = query(
@@ -27,6 +30,7 @@ export default function BusinessListsByCategory() {
     querySnapshot.forEach((doc) => {
       seBusinesses((prev) => [...prev, doc.data() as Business]);
     });
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -39,10 +43,16 @@ export default function BusinessListsByCategory() {
 
   return (
     <View>
-      {businesses?.length > 0 ? (
+      {businesses?.length > 0 && !isLoading ? (
         <FlatList
           data={businesses}
           renderItem={({ item }) => <BusinessListItem item={item} />}
+        />
+      ) : isLoading ? (
+        <ActivityIndicator
+          style={{ marginTop: "50%" }}
+          size={"large"}
+          color={Colors.primary}
         />
       ) : (
         <Text
